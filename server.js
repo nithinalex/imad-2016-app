ad70473f529e078c11f66237772665908ecde2e7
nithinalex <nithinalexjohn@gmail.com>
2016-10-08 15:14:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,50 +25,63 @@ var articleOne = {
 }
 
 
-var htmlTemplate = 
-`    
-    <html>
+Function createTemplate (data){
             
-        <head>
-                
-            <title>
-                   Article One: Nithin Alex John
-            </title>
-                
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            var title = data.title;
+            var date = data.date;
+            var heading = data.heading;
+            var content = data.content;
             
-            <link href="/ui/style.css" rel="stylesheet" />
-                
-                
-         </head>   
-                
-        <body>
+                    var htmlTemplate = 
+                    `    
+                        <html>
+                                
+                            <head>
+                                    
+                                <title>
+                                       ${title}
+                                </title>
+                                    
+                                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                                
+                                <link href="/ui/style.css" rel="stylesheet" />
+                                    
+                                    
+                             </head>   
+                                    
+                            <body>
+                                        
+                                <div class="container">
+                            
+                                    <div>
+                                       <a href="/">Home</a>  
+                                    </div>
+                                                
+                                        <hr/>
                     
-            <div class="container">
-        
-                <div>
-                   <a href="/">Home</a>  
-                </div>
-                            
-                    <hr/>
-
-                <h3>
-                    Article One 
-                </h3>
-                            
-                <div>
-                    Sep 26, 2016
-                </div>
-                            
-            <div>
-                                
-                                
-        </body>
-                
-    </html>
-
-`    
-}
+                                    <h3>
+                                        ${heading} 
+                                    </h3>
+                                                
+                                    <div>
+                                        ${date}
+                                    </div>
+                                    
+                                     <div>
+                                        ${content}
+                                    </div>
+                                                
+                                <div>
+                                                    
+                                                    
+                            </body>
+                                    
+                        </html>
+                    
+                    `    
+                    
+        return htmlTemplate;            
+}            
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -76,7 +89,8 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));    
+//res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
